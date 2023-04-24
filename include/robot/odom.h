f148debdf0d328397385c92527472536ad80868c
Shaun Kyle <shaun@jpk.co.nz>
2023-04-24 12:26:49 +1200

    define odometry subsystem methods

--- include/robot/odom.h
+++ include/robot/odom.h
@@ -5,26 +5,15 @@
 
 // Data structure used for Points on 2D coordinate frames
 typedef struct {
-  double x;     // in distance unit (e.g. tiles, cm)
-  double y;
-  double theta; // in [deg]?
+  double x;     // Horizontal coordinate [mm]
+  double y;     // Vertical coordinate [mm]
+  double theta; // Heading [deg]
 } Pose2D_t;
 
-// Data structure used for vectors?
-
-// Odometry functions
-//setStartPose  // Where is robot starting on global frame?
-//init    // Calculates offset of SBF frame from global frame, based on start pose
-//double odomFindDistance(p_start, p_end)
-//double odomFindAngle(p_start, p_end)
-//
-
-
-double degreesToRadians(double degrees);
 void odomInit();
+void odomPeriodic();
 
-void odomSetGlobalFrame(double x, double y, double theta);
-
-void odomUpdateGlobalPosition(void);
+double odomFindDistance(Pose2D_t p_begin, Pose2D_t p_end);
+double odomFindHeading(Pose2D_t p_begin, Pose2D_t p_end);
 
 #endif // ROBOT_ODOM_H
