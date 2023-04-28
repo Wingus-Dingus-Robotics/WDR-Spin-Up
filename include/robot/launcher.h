87b412404b441b25e78b65ada010c64558f36bb0
Shaun Kyle <shaun@jpk.co.nz>
2023-04-27 23:29:29 -0500

    launcher PD control it good now.

--- include/robot/launcher.h
+++ include/robot/launcher.h
@@ -2,15 +2,16 @@
 #define ROBOT_LAUNCHER_H
 
 #include "v5_apitypes.h"
+#include "util/control.h"
 
 #define LAUNCHER_ENC_TO_REV 50
 #define LAUNCHER_RAMP_INCREMENT_PWM 1
 #define RPM_BUFFER_SIZE 50
 
-#define LAUNCHER_KP 0.5
-#define LAUNCHER_KI 0.05
-#define LAUNCHER_WINDUP 1000.0
-#define LAUNCHER_KD 0.0
+#define LAUNCHER_KP 1.5
+#define LAUNCHER_KI 0.0
+#define LAUNCHER_WINDUP 0.0
+#define LAUNCHER_KD 0.05
 #define LAUNCHER_DT 0.001
 
 typedef struct {
@@ -22,6 +23,9 @@ extern launcher_speeds_t LAUNCHER_SPEED_LOW;
 extern launcher_speeds_t LAUNCHER_SPEED_MED;
 extern launcher_speeds_t LAUNCHER_SPEED_HIGH;
 extern launcher_speeds_t LAUNCHER_SPEED_WALL;
+
+// Hack to view PID output in display.cpp
+extern PID_Controller_t speed_pid_L, speed_pid_R;
 
 void launcherInit(void);
 void launcherPeriodic(void);
